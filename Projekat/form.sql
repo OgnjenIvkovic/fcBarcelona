a269f58497f1b4ee1bfef2d22767c21f2f027f9e
OgnjenIvkovic <ognjenivkovicit@gmail.com>
2024-11-19 15:45:09 +0100

    Add files via upload

---
+++ Projekat/form.sql
@@ -0,0 +1,95 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 08, 2024 at 01:33 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `insert`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `form`
+--
+
+CREATE TABLE `form` (
+  `ime` varchar(250) NOT NULL,
+  `email` varchar(250) NOT NULL,
+  `comment` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `form`
+--
+
+INSERT INTO `form` (`ime`, `email`, `comment`) VALUES
+('ognjen', 'ogiivkovic15@gmail.com', 'sddadsdad'),
+('ognjen', 'ogiivkovic15@gmail.com', 'sddadsdad'),
+('ognjen', 'angela.djurka@gmail.com', 'rgfdgfgd'),
+('ognjen', 'angela.djurka@gmail.com', 'rgfdgfgd'),
+('ognjen', 'angela.djurka@gmail.com', 'rgfdgfgd'),
+('ognjen', 'angela.djurka@gmail.com', 'rgfdgfgd'),
+('ognjen', 'angela.djurka@gmail.com', 'rgfdgfgd'),
+('ognjen', 'angela.djurka@gmail.com', 'rgfdgfgd'),
+('ognjen', 'ogiivkovic15@gmail.com', 'vdgdfg'),
+('ognjen', 'angela.djurka@gmail.com', 'dsdadsdd'),
+('sa', 'angela.djurka@gmail.com', 'dsdadsdd'),
+('RADI', 'angela.djurka@gmail.com', 'dsdadsdd'),
+('DSFFSF', 'ogiivkovic15@gmail.com', 'DFDFDS'),
+('DSFFSF', 'ogiivkovic15@gmail.com', 'wddads'),
+('DSFFSF', 'ogiivkovic15@gmail.com', 'dasdsds'),
+('RADI', 'ogiivkovic15@gmail.com', 'czxcxc'),
+('RADI', 'ogiivkovic15@gmail.com', 'czxcxc'),
+('RADI', 'ogiivkovic15@gmail.com', 'czxcxc'),
+('RADI', 'ogiivkovic15@gmail.com', 'czxcxc'),
+('DSFFSF', 'ogiivkovic15@gmail.com', 'dasdsds'),
+('DSFFSF', 'ogiivkovic15@gmail.com', 'dasdsds'),
+('DSFFSF', 'ogiivkovic15@gmail.com', 'dasdsds'),
+('DSFFSF', 'ogiivkovic15@gmail.com', 'dasdsds'),
+('DSFFSF', 'ogiivkovic15@gmail.com', 'dasdsds'),
+('DSFFSF', 'ogiivkovic15@gmail.com', 'dasdsds'),
+('DSFFSF', 'ogiivkovic15@gmail.com', 'dasdsds'),
+('DSFFSF', 'ogiivkovic15@gmail.com', 'dasdsds'),
+('DSFFSF', 'ogiivkovic15@gmail.com', 'dasdsds'),
+('DSFFSF', 'ogiivkovic15@gmail.com', 'dasdsds'),
+('DSFFSF', 'ogiivkovic15@gmail.com', 'dasdsds'),
+('DSFFSF', 'ogiivkovic15@gmail.com', 'dasdsds'),
+('DSFFSF', 'ogiivkovic15@gmail.com', 'dasdsds'),
+('DSFFSF', 'ogiivkovic15@gmail.com', 'dasdsds'),
+('DSFFSF', 'ogiivkovic15@gmail.com', 'dasdsds'),
+('DSFFSF', 'ogiivkovic15@gmail.com', 'dasdsds'),
+('DSFFSF', 'ogiivkovic15@gmail.com', 'dasdsds'),
+('DSFFSF', 'ogiivkovic15@gmail.com', 'dasdsds'),
+('DSFFSF', 'ogiivkovic15@gmail.com', 'dasdsds'),
+('DSFFSF', 'ogiivkovic15@gmail.com', 'dasdsds'),
+('DSFFSF', 'ogiivkovic15@gmail.com', 'dasdsds'),
+('DSFFSF', 'ogiivkovic15@gmail.com', 'dasdsds'),
+('DSFFSF', 'ogiivkovic15@gmail.com', 'dasdsds'),
+('DSFFSF', 'ogiivkovic15@gmail.com', 'dasdsds'),
+('DSFFSF', 'ogiivkovic15@gmail.com', 'dasdsds'),
+('DSFFSF', 'ogiivkovic15@gmail.com', 'dasdsds'),
+('DSFFSF', 'ogiivkovic15@gmail.com', 'dasdsds'),
+('DSFFSF', 'ogiivkovic15@gmail.com', 'dasdsds'),
+('DSFFSF', 'ogiivkovic15@gmail.com', 'dasdsds'),
+('DSFFSF', 'ogiivkovic15@gmail.com', 'dasdsds'),
+('DSFFSF', 'ogiivkovic15@gmail.com', 'dasdsds');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
